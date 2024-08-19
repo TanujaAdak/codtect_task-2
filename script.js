8d187a2bf828247d526e3270f6ae2d6264a388d0
TanujaAdak <adaktanuja@gmail.com>
2024-08-19 15:55:27 +0530

    weather forecast

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
 const apikey = "b88be1a59dd9473039e2f3d9764c637e";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=raipur";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apikey}`);
-    var data = await response.json();
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-    console.log(data);
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else {
+
+        var data = await response.json();
+
+        console.log(data);
 
 
 
 
-document.querySelector(".city").innerHTML = data.name;
-document.querySelector(".temp").innerHTML =Math.round(data.main.temp)  + " °C ";
-document.querySelector(".humidity").innerHTML = data.main.humidity + " % ";
-document.querySelector(".wind").innerHTML = data.wind.speed + " km/h ";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C ";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " % ";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h ";
+
+
+        if (data.weather[0].main == "Haze") {
+            weatherIcon.src = "haze.png";
+        }
+        else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "sun.png";
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "rain.png";
+        }
+        else if (data.weather[0].main == "Drizzel") {
+            weatherIcon.src = "drizzel.png";
+        }
+        else if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "clouds.png";
+        }
+        else if (data.weather[0].main == "Cloudy") {
+            weatherIcon.src = "cloudy.png";
+        }
+
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
 
 }
-checkWeather();
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keydown", (event) => {
+    if (event.key === "Enter")
+    checkWeather(searchBox.value);
+});
